fa93aeec8b47ff2d5e11107a1c1ece935c2feef5
antonio <tonyldo@gmail.com>
2018-04-19 20:55:04 -0300

    main upgrade

--- main.py
+++ main.py
@@ -1,67 +1,73 @@
 import connectWifi
 import umqttClient
-import time
+import micropython
+
 from machine import Pin
+from machine import Timer
+
+micropython.alloc_emergency_exception_buf(100)
 
 PIN_OUT = const(2)
 PIN_IN  = const(0)
 
 pinOut = None
 pinIn = None
-pinInValue = None
+newValue = None
+command = None
+timer = None
 
 STATE_LAMP_TOPIC  = b'/lamp/state'
 SWITCH_LAMP_TOPIC = b'/lamp/switch'
 
 DICT_MSG_IN = {b'ON': 0, b'OFF' : 1}
 DICT_MSG_OUT = {0: b'ON', 1 : b'OFF'}
-mqttMsg = None
-
-mqttCommand = None
-pressButtonCommand = None
-setupCommand = None
-
-def cleanCommands():
-    print ('Clean Commands!')
-    global mqttCommand, pressButtonCommand, setupCommand, mqttMsg
-    mqttCommand = False
-    pressButtonCommand = False
-    setupCommand = False
-    mqttMsg = b'NONE'
-    print ('Waiting for new commands...')
 
 def setupPins():
     global pinOut, pinIn
     if pinOut is not None or pinIn is not None:
        return
-
     print ('Setup Pins...')
-    pinOut = Pin(PIN_OUT,Pin.OUT)
+    pinOut = Pin(PIN_OUT,Pin.OUT,value=1)
     pinIn = Pin(PIN_IN,Pin.IN)
     pinIn.irq(handler=pinInPressCallback)
-    pinOut.value(pinIn.value())
+
+def setupTimer():
+    global timer
+    if timer is None:
+       timer = Timer(-1)
+       timer.init(period=2000, mode=Timer.PERIODIC, callback=timerCallback)
+
+def cleanCommand():
+    global command
+    command = 'pass'
 
 def pinInPressCallback(pin):
-    global pressButtonCommand,pinInValue
-    pressButtonCommand = True
-    #pinInValue = pin.value()
-    pinInValue = int(not pinOut.value())
+    global command,newValue
+    command = 'toggleByPress'
+    newValue = int(not pinOut.value())
+
+def timerCallback(timer):
+    global command
+    command = 'check'
 
 def mqttSubcristionCallback(topic,msg):
-    global mqttCommand,mqttMsg
-    mqttCommand = True
-    mqttMsg = msg
+    global command,newValue
+    command = 'toggleByMQTT'
+    newValue = DICT_MSG_IN[msg]
 
-def sendMQTTStateLamp():
-     print ('Enter sendMQTTStateLamp method, pinOUt state: ', DICT_MSG_OUT[pinOut.value()])
-     umqttClient.getMQTT().publish(STATE_LAMP_TOPIC,DICT_MSG_OUT[pinOut.value()])
+def checkAndSendeMQTTMsg():
+    umqttClient.getMQTT().check_msg()
+    if (command == 'toggleByMQTT'):
+       return
+    print ('Enter sendMQTTStateLamp method, pinOUt state: ', DICT_MSG_OUT[pinOut.value()])
+    umqttClient.getMQTT().publish(STATE_LAMP_TOPIC,DICT_MSG_OUT[pinOut.value()])
+    cleanCommand()
 
-def toogleSwitch(newValue):
-    print ('Enter toogleSwitch method, newValue / pinOut.value(): ',newValue,' / ',pinOut.value())
+def toggleSwitch():
+    print ('Enter toogleSwitch method, newValue / pinOut.value(): ', DICT_MSG_OUT[newValue],' / ',DICT_MSG_OUT[pinOut.value()])
     if newValue!=pinOut.value():
        pinOut.value(newValue)
-       sendMQTTStateLamp()
-    cleanCommands()
+    cleanCommand()
 
 def setup():
     print ('Enter setup method...')
@@ -69,33 +75,23 @@ def setup():
     connectWifi.connect()
     umqttClient.getMQTT(callbackFunction=mqttSubcristionCallback).connect()
     umqttClient.getMQTT().subscribe(SWITCH_LAMP_TOPIC)
-    sendMQTTStateLamp()
-    cleanCommands()
+    setupTimer()
+    cleanCommand()
 
 def main():
     print ('Enter main method...')
-    global setupCommand
-    setupCommand = True
-
-    while True:
+    global command
+    command = 'setup'     
+    try:
+       while True:
           try:
-             if setupCommand:
-                setup()
-             elif mqttCommand and mqttMsg in DICT_MSG_IN:
-                print ('Executing MQTT command!')
-                toogleSwitch(DICT_MSG_IN[mqttMsg])
-             elif pressButtonCommand:
-                print ('Executing Button command!')
-                toogleSwitch(pinInValue)
-             else:
-                pass
-
-             umqttClient.getMQTT().check_msg()
+              {'setup':setup, 'toggleByPress':toggleSwitch, 'toggleByMQTT':toggleSwitch, 'check':checkAndSendeMQTTMsg, 'pass':lambda : None}[command]()
           except Exception as e:
-             print ('Error: ',e)
-             setupCommand = True
-
-          time.sleep(1)
+              print ('Error: ',e)
+              command = 'setup'
+    finally:
+       umqttClient.getMQTT().disconnect()
+       connectWifi.disconnect()
 
 if (__name__) == '__main__':
    main()
